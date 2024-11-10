586c93cfc49bb52957e579c8c6cc299dd3ca99e2
Adam Malewski <adam.malewski@wp.pl>
2024-11-10 12:31:52 +0100

    Home chore repository setup

---
+++ MyProject.Application/HomeChores/IHomeChoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyProject.Domain.Entities;
+
+namespace MyProject.Application.HomeChores;
+
+public interface IHomeChoreRepository
+{
+    Task<IList<HomeChore>> GetAllAsync();
+    Task<HomeChore> GetByIdAsync(int id);
+    Task AddAsync(HomeChore homeChore);
+    Task UpdateAsync(HomeChore homeChore);
+    Task DeleteAsync(int id);
+}
---
+++ MyProject.Infrastructure/Repositories/HomeChoreRepository.cs
@@ -0,0 +1,40 @@
+﻿using MyProject.Application.Common.Interfaces;
+using MyProject.Application.HomeChores;
+using MyProject.Domain.Entities;
+
+namespace MyProject.Infrastructure.Repositories;
+
+public class HomeChoreRepository : IHomeChoreRepository
+{
+    private IApplicationDbContext _db;
+
+    public HomeChoreRepository(IApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public Task AddAsync(HomeChore homeChore)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<HomeChore>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<HomeChore> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(HomeChore homeChore)
+    {
+        throw new NotImplementedException();
+    }
+}
